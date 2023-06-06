a43c6dadb1c2f50782e73fb49bd43c425f5ffd85
who cares <doesntexist@localhost.local>
2023-06-06 14:08:58 +0200

    Ved brug af hacks, har jeg fået programmet til et tilstand, hvor vi kan begynde arbejde på API.

--- Program.cs
+++ Program.cs
@@ -128,9 +128,10 @@ namespace Vinimport_TUI
             int y_accounted_for_bar = current_windowheight - bottom_bar_height;
             int horizontally_middle = (y_accounted_for_bar - 1) / 2;
             int vertically_middle = (current_windowwidth - 1) / 2;
+            int where_to_write = 0;
 
             // Mest højeste og venstreste punktet af fielder.
-            field_size = new int[3, 2] {{0, 0}, {0, horizontally_middle + 1}, {vertically_middle + 1, 0 } };
+            field_size = new int[3, 2] { { 0, 0 }, { 0, horizontally_middle + 1 }, { vertically_middle + 1, 0 } };
             /* // Langere alternativ:
             field_size[0, 0] = 0; 
             field_size[0, 1] = 0;
@@ -166,19 +167,59 @@ namespace Vinimport_TUI
             {
                 for (int second_dim = 0; second_dim < text_fields[first_dim].Length; ++second_dim)
                 {
-                   if (second_dim == 0)
-                   {
-                        Console.SetCursorPosition( centered_text(vertically_middle, text_fields[first_dim][second_dim]), field_size[first_dim, 1] + 1 );
+                    if (second_dim == 0)
+                    {
+                        Console.SetCursorPosition(centered_text(vertically_middle, text_fields[first_dim][second_dim]) + field_size[first_dim, 0] + 1, field_size[first_dim, 1] + 1);
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.Write(text_fields[first_dim][second_dim] + "\n");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else if (text_fields[first_dim][second_dim] == "sep")
+                    {
+                        Console.SetCursorPosition(field_size[first_dim, 0] + 1, Console.CursorTop + 1);
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.Write(new string('-', vertically_middle - 1));
+                        Console.ForegroundColor = ConsoleColor.White;
+                        //Snyd, sep er kun brugt i tredje subvindue, derfor den har ikke brug for switch
+                        Console.Write(new string('\n', 5));
+
+                    }
+                    else
+                    {
+                        Console.SetCursorPosition(field_size[first_dim, 0] + 1, Console.CursorTop);
                         Console.ForegroundColor = ConsoleColor.Cyan;
                         Console.Write(text_fields[first_dim][second_dim]);
                         Console.ForegroundColor = ConsoleColor.White;
-                   }
-
+                        pos_of_inputs[where_to_write, 0] = field_size[first_dim, 0];
+                        if (second_dim + 1 < text_fields[first_dim].Length && first_dim == 2) //også snyd
+                        {
+                            if (text_fields[first_dim][second_dim + 1] == "sep")
+                            {
+                                pos_of_inputs[where_to_write, 1] = Console.CursorTop + 2;
+                                where_to_write += 1;
+                            }
+                            else
+                            {
+                                pos_of_inputs[where_to_write, 1] = Console.CursorTop + 1;
+                                where_to_write += 1;
+                            }
+                        }
+                        else
+                        {
+                            pos_of_inputs[where_to_write, 1] = Console.CursorTop + 1;
+                            where_to_write += 1;
+                            //Snyd, den skal bare virke
+                            if (first_dim == 0)
+                                Console.Write(new string('\n', 4));
+                            else if (first_dim == 1)
+                                Console.Write(new string('\n', 3));
+                        }
+                    }
                 }
             }
 
-
-
+            pos_of_inputs[8,0] = 0;
+            pos_of_inputs[8,1] = Console.WindowHeight - 1;
         }
         static void Main(string[] args)
         {
@@ -202,28 +243,45 @@ namespace Vinimport_TUI
                 if (current_windowwidth != Console.WindowHeight || current_windowheight != Console.WindowWidth)
                     generate_ui(); //regeneration af ui
 
-                //* //Debug
-                string lol = Console.WindowWidth + " " + Console.WindowHeight;
-                Console.SetCursorPosition(centered_text(Console.WindowWidth, lol), Console.WindowHeight - 2);
+                /* //Debug
+                Console.SetCursorPosition(0, Console.WindowHeight - 2);
                 Console.Write(Console.WindowWidth + " " + Console.WindowHeight);
+                //*/
+                input_fields("newsfeed", new string[1] { "lol" });
+                /*
+                Console.Clear();
+                for (int a = 0; a < pos_of_inputs.GetLength(0); ++ a)
+                {
+                    Console.WriteLine(a + " = " + pos_of_inputs[a,0] + ", " + pos_of_inputs[a,1] );
+                }
+                break;
                 //*/
 
                 /* API:
-                Du skal bruge "input_fields(where, what)" til at få ting på skærmen,
+                Du skal bruge "input_fields("where", what)" til at få ting på skærmen,
                 "what" er string array, som bevæge cursor til specifiske plads og skrive hvad der er i array.
                 "where" referere til et switch statement, som tjekke, om en af de følgene tekstfælde passer, som du kende som:
-                "temp_og_fugt_lager"    = "Lager:"
-                "temp_og_fugt_udenfor"  = "Udenfor:"
-                "date_kobenhavn"        = "København:"
-                "date_london"           = "London:"
-                "date_singapore"        = "Singapore:"
-                "lager_min"             = "Varer under minimum"
-                "lager_max"             = "Varer over maksimum"
-                "lager_mest"            = "Mest solgte i dag"
-                "newsfeed"              = "Newsfeed fra Nordjyske"
+                0. "temp_og_fugt_lager"    = "Lager:"
+                1. "temp_og_fugt_udenfor"  = "Udenfor:"
+                2. "date_kobenhavn"        = "København:"
+                3. "date_london"           = "London:"
+                4. "date_singapore"        = "Singapore:"
+                5. "lager_min"             = "Varer under minimum"
+                6. "lager_max"             = "Varer over maksimum"
+                7. "lager_mest"            = "Mest solgte i dag"
+                8. "newsfeed"              = "Newsfeed fra Nordjyske"
 
+                EKSEMPLER:
+                    uden string af arrayer:
+                        input_fields("newsfeed", new string[1] { "lol" });
+                        set_and_write(8, new string[1] { "lol" });
+                    med string af arrayer:
+                        string[] var = new string[] {"lol"};
+
+                        input_fields("newsfeed", var);
+                        set_and_write(8, var);
+                
                 VÆR OPMARKSOM: input_fields tjekke ikke for hvor ofte sender du requester til API
-
                 */
 
             }
